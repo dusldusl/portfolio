28fccfb968eea8b5c0c71dc2d2ca468af1793410
yeonhee <cyh2616@naver.com>
2025-02-17 00:07:52 +0900

    기본 틀 만들기_1

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <h1>
+                채연희 
+            </h1>
+        </div>
+    )
+}
+
+export default Header;
